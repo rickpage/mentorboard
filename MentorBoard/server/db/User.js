300d8f2cce5927885566087c89bb7201392912fc
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-07-18 22:14:33 -0400

    login on click working and message board working as expected

--- MentorBoard/server/db/User.js
+++ MentorBoard/server/db/User.js
@@ -6,26 +6,27 @@ mongoose.connect('mongodb://localhost:1234/messageboard')
   .catch((err) => console.error(err));
 
 
-const UserSchema = new mongoose.Schema({
-    userName:{
-        type: String,
-        default:'',
-    },
-    password: {
-        type: String,
-        default:'',
-    },
-    isDeleted: {
-        type: Boolean,
-        default: false,
-    }
-});
-UserSchema.methods.generateHash = function(password){
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
-}
-
-UserSchema.methods.validPassword = function(password){
-    return bcrypt.compareSync(password, this.password)
-}
+  var bcrypt = require('bcrypt');
+  var UserSchema = mongoose.Schema({
+      local: {
+          username: String,
+          password: String
+      },
+      facebook: {
+          id: String,
+          token: String,
+          email: String,
+          name: String
+      }
+  });
+  
+  UserSchema.methods.generateHash = function(password){
+      return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
+  }
+  
+  UserSchema.methods.validPassword = function(password){
+      return bcrypt.compareSync(password, this.local.password);
+  }
+  
 
 module.exports = mongoose.model('User', UserSchema);
--- MentorBoard/server/index.js
+++ MentorBoard/server/index.js
@@ -17,9 +17,9 @@ app.get('/', (req, res) => {
     message: 'messageboard'
   });
 });
-app.get('/login', (req, res) => {
-  res.json();
-});
+// app.get('/login', (req, res) => {
+//   res.json();
+// });
 app.get('/messages', (req, res) => {
   messages.getAll().then((messages) => {
     res.json(messages);
@@ -36,18 +36,67 @@ app.post('/messages', (req, res) => {
   });
 });
 
-app.post('/register', (req, res) => {
-  User.create(req.body)
-  .then(function(dbUser) {
-    console.log(dbUser);
-    res.json(dbUser);
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+// app.post('/register', (req, res) => {
+//   User.create(req.body)
+//   .then(function(dbUser) {
+//     console.log(dbUser);
+//     res.json(dbUser);
+//   })
+//   .catch(function(err) {
+//     res.json(err);
+//   });
   
-})
+// })
 
+module.exports = function(app, passport){
+	app.get('/', function(req, res){
+		res.render('index.ejs');
+	});
+
+	app.get('/login', function(req, res){
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
+
+	app.get('/signup', function(req, res){
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/',
+		failureRedirect: '/signup',
+		failureFlash: true
+	}));
+
+	app.get('/profile', isLoggedIn, function(req, res){
+		res.render('profile.ejs', { user: req.user });
+	});
+
+	app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
+
+	app.get('/auth/facebook/callback', 
+	  passport.authenticate('facebook', { successRedirect: '/profile',
+	                                      failureRedirect: '/' }));
+
+
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	})
+};
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+
+	res.redirect('/login');
+}
 const port = process.env.PORT || 1234;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
